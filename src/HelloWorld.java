b91ef542d7b3d5af0592af7d41a23deda8aed184
Amir Hossein Garousi <amirgarousi73@gmail.com>
2025-03-22 17:01:20 +0330

    add source code

---
+++ src/HelloWorld.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java 21.");
+        System.out.println("sum = " + sum(args));
+        System.out.println(args[0] + args[1]);
+
+        System.out.printf("Result = %.3f", multiply());
+
+    }
+
+    private static int sum(String[] args) {
+        return Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+    }
+
+    private static int testScanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 2 number for multiplication: ");
+        return scanner.nextInt() * scanner.nextInt();
+    }
+
+    private static double multiply() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter two number: ");
+        return scanner.nextDouble() * scanner.nextDouble();
+    }
+}
